f56c934c3b93bb8603b1b02857da892ce8e0d658
PauloBorszcz <paulofelipeborszcz2004@gmail.com>
2025-03-28 10:10:50 -0300

    chore: remove cached files to normalize line endings

--- src/types/discord.ts
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
-import { DiscordService } from "../services/DiscordService";
-
-export interface Command {
-  data: SlashCommandBuilder;
-  execute: (interaction: CommandInteraction, discordService: DiscordService) => Promise<void>;
-}
